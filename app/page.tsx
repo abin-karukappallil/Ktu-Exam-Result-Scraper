1f8e9805e95093bef0826d29e7b16a8f78105ab1
Abin Thomas <73917119+abin-karukappallil@users.noreply.github.com>
2026-03-21 20:48:43 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <ResultDashboard
       title="KTU Exam Result Scraper"
-      subtitle="Fetch your semester grade card data directly...no more unwanted clicks..get the result and cry"
+      subtitle="Fetch your semester grade card data directly...no more unwanted clicks..get the result."
     />
   );
 }
